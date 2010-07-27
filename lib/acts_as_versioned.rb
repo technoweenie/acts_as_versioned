efc726d055ac75e3684b7272561e7f9d95735738
Brian Rose & Mike Barinek <pair+heimidal+mbarinek@pivotallabs.com>
2010-07-28 05:18:58 +0800

    revert_to now writes attributes via hash-style assignment instead of directly in order to prevent running a custom setter on a value twice.

--- lib/acts_as_versioned.rb
+++ lib/acts_as_versioned.rb
@@ -332,7 +332,7 @@ module ActiveRecord #:nodoc:
           # Clones a model.  Used when saving a new version or reverting a model's version.
           def clone_versioned_model(orig_model, new_model)
             self.class.versioned_columns.each do |col|
-              new_model.send("#{col.name}=", orig_model.send(col.name)) if orig_model.has_attribute?(col.name)
+              new_model[col.name] = orig_model.send(col.name) if orig_model.has_attribute?(col.name)
             end
 
             if orig_model.is_a?(self.class.versioned_class)
